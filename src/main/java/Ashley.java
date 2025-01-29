a15c6250fcfc869880a0d26fe6241fc0f99cb288
ivan-tan <tanjunhaoivan@gmail.com>
2025-01-30 01:41:51 +0800

    Add echo function

--- src/main/java/Ashley.java
+++ src/main/java/Ashley.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
+
 public class Ashley {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Ashley");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------------------------------");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (line != null){
+            if (line.equals("bye")){
+                System.out.println("        Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("        " + line);
+            line = in.nextLine();
+        }
     }
 }
