f12d246a149d2757789956e84501e0478b00ae46
ivan-tan <tanjunhaoivan@gmail.com>
2025-02-02 23:46:41 +0800

    Update Ashley by refactoring
    Refactored line-seperator

--- src/main/java/Ashley.java
+++ src/main/java/Ashley.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ashley {
+    public static final String LINE_SEPERATOR = "---------------------------------------------------";
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Ashley");
         System.out.println("What can I do for you?");
-        System.out.println("---------------------------------------------------");
+        System.out.println(LINE_SEPERATOR);
 
         String line;
         Scanner in = new Scanner(System.in);
