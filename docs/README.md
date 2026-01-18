b4a05ff67f9f1029e5a58dcebb69d86e51716bf6
ivan-tan <tanjunhaoivan@gmail.com>
2026-01-19 00:26:26 +0800

    Set up chatbot. Rename Duke to Ashley

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Ashley User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Ashley.java
@@ -0,0 +1,11 @@
+public class Ashley {
+    public static final String LINE_SEPERATOR = "---------------------------------------------------";
+    public static void main(String[] args) {
+        System.out.println(LINE_SEPERATOR);
+        System.out.println("Hello! I'm Ashley");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE_SEPERATOR);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE_SEPERATOR);
+    }
+}
